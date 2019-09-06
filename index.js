670d7904ca4264c9705accacfef80ce2cb459cb7
caipinfang <caipinfang@meituan.com>
2019-09-06 19:02:48 +0800

    函数防抖

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// 函数防抖
-
-/*--------函数节流--------*/
+/*------------------------函数节流--start----------------------*/
 // CD 冷却时间  游戏中释放某个技能后，3秒内或某个时间内不能再释放某个技能
 
 function fn() {
@@ -29,3 +27,24 @@ btn1.onclick = function() {
     }, 3000);
   }
 };
+/*------------------------函数节流--end----------------------*/
+
+/*------------------------函数防抖--start----------------------*/
+//带着一起做
+function fn2() {
+  console.log("函数防抖按钮测试");
+}
+var btn2 = document.getElementById("button2");
+
+var timerId = null;
+btn2.onclick = function() {
+  if (timerId) {
+    window.clearTimeout(timerId);
+  }
+  timerId = setTimeout(() => {
+    fn();
+    timerId = null;
+  }, 5000);
+};
+
+/*------------------------函数防抖--start----------------------*/
